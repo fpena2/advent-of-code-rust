ca399890089ba34888e095fccf7da9549241eaff
fpena2 <f.j.pena@wustl.edu>
2024-12-01 00:34:03 -0600

    Day1 Part2

--- src/bin/01.rs
+++ src/bin/01.rs
@@ -22,7 +22,22 @@ pub fn part_one(input: &str) -> Option<u32> {
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
-    None
+    let mut left: Vec<u32> = vec![];
+    let mut right: Vec<u32> = vec![];
+
+    for line in input.lines() {
+        let pair: Vec<u32> = line.split("   ").map(|v| v.parse().unwrap()).collect();
+        left.push(pair[0]);
+        right.push(pair[1]);
+    }
+
+    let mut sum = 0;
+    for l in left.iter() {
+        let occurrences = right.iter().filter(|val| val == &l).count();
+        sum += l * occurrences as u32;
+    }
+
+    Some(sum)
 }
 
 #[cfg(test)]
@@ -38,6 +53,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(31));
     }
 }
