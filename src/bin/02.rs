76aaf110b8b543ea508472900298a36c19c33484
fpena2 <f.j.pena@wustl.edu>
2024-12-02 19:10:03 -0600

    d2p1

---
+++ src/bin/02.rs
@@ -0,0 +1,63 @@
+advent_of_code::solution!(2);
+
+pub fn is_safe(levels: Vec<i32>) -> bool {
+    let mut prev_cal: i32 = 0;
+    for pos in 0..levels.len() - 1 {
+        let cal = levels[pos] - levels[pos + 1];
+
+        if cal.abs() > 3 || cal == 0 {
+            return false;
+        }
+
+        if (prev_cal.is_negative() && cal.is_positive())
+            || (prev_cal.is_positive() && cal.is_negative())
+        {
+            if pos == 0 {
+                continue;
+            }
+
+            return false;
+        }
+
+        prev_cal = cal;
+    }
+
+    return true;
+}
+
+pub fn part_one(input: &str) -> Option<u32> {
+    let mut safe_levels = 0;
+    for line in input.lines() {
+        let level: Vec<i32> = line
+            .split(" ")
+            .map(|n| n.parse::<i32>().unwrap())
+            .collect::<Vec<i32>>();
+
+        if is_safe(level) {
+            safe_levels += 1;
+        }
+    }
+
+    Some(safe_levels)
+}
+
+pub fn part_two(input: &str) -> Option<u32> {
+    None
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part_one() {
+        let result = part_one(&advent_of_code::template::read_file("examples", DAY));
+        assert_eq!(result, Some(2));
+    }
+
+    #[test]
+    fn test_part_two() {
+        let result = part_two(&advent_of_code::template::read_file("examples", DAY));
+        assert_eq!(result, None);
+    }
+}
