d13212bee770f48a50e32e2611b814a0dd93020f
fpena2 <f.j.pena@wustl.edu>
2024-12-08 03:14:11 -0600

    d8p2

--- src/bin/08.rs
+++ src/bin/08.rs
@@ -64,8 +64,46 @@ pub fn part_one(input: &str) -> Option<u32> {
     Some(antinodes.len() as u32)
 }
 
+fn find_harmonic_antinodes(antennas: &HashMap<(usize, usize), char>) -> HashSet<(usize, usize)> {
+    let mut antinodes: HashSet<(usize, usize)> = HashSet::new();
+    for (antenna_position, antenna_type) in antennas.iter() {
+        // println!("antenna: ({},{})", antenna_position.0, antenna_position.1);
+        let other_antennas: Vec<(&(usize, usize), &char)> = antennas
+            .iter()
+            .filter(|a| a.0 != antenna_position && a.1 == antenna_type)
+            .collect::<Vec<(&(usize, usize), &char)>>();
+
+        for (other_antenna_position, _) in other_antennas {
+            let x_diff = antenna_position.0 as isize - other_antenna_position.0 as isize;
+            let y_diff = antenna_position.1 as isize - other_antenna_position.1 as isize;
+
+            let mut antinode;
+
+            // FIXME:Hardcoded calculations of antinodes
+            for i in 1..100 {
+                antinode = (
+                    antenna_position.0 as isize + x_diff * i,
+                    antenna_position.1 as isize + y_diff * i,
+                );
+                antinodes.insert((antinode.0 as usize, antinode.1 as usize));
+            }
+        }
+
+        // Antennas are also antinodes
+        antinodes.insert(*antenna_position);
+    }
+    antinodes
+}
+
 pub fn part_two(input: &str) -> Option<u32> {
-    None
+    let grid = input_to_matrix(input);
+    let antennas = find_antennas(&grid);
+    let antinodes = find_harmonic_antinodes(&antennas)
+        .into_iter()
+        .filter(|n| n.0 < grid[0].len() && n.1 < grid.len())
+        .collect::<HashSet<(usize, usize)>>();
+    // println!("Antinodes: {:?}", antinodes);
+    Some(antinodes.len() as u32)
 }
 
 #[cfg(test)]
@@ -81,6 +119,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(34));
     }
 }
