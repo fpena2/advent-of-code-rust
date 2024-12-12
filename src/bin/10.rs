620bf52575ade0c400541e0bb8c0d05437256ae1
fpena2 <f.j.pena@wustl.edu>
2024-12-12 06:57:47 -0600

    d10p2

--- src/bin/10.rs
+++ src/bin/10.rs
@@ -1,6 +1,6 @@
 advent_of_code::solution!(10);
 
-use std::collections::{HashSet, VecDeque};
+use std::collections::{HashMap, HashSet, VecDeque};
 
 fn input_to_matrix(input: &str) -> Vec<Vec<usize>> {
     let mut grid: Vec<Vec<usize>> = Vec::new();
@@ -108,8 +108,66 @@ pub fn part_one(input: &str) -> Option<u32> {
     Some(result)
 }
 
-pub fn part_two(input: &str) -> Option<u32> {
-    None
+fn score_unique_trails_to_trailend(trailhead: &(usize, usize), grid: &Vec<Vec<usize>>) -> usize {
+    let mut deque: VecDeque<(usize, usize)> = VecDeque::new();
+    let mut seen: HashMap<(usize, usize), usize> = HashMap::new();
+
+    deque.push_back(*trailhead);
+    seen.insert(*trailhead, 1);
+
+    let mut trails = 0usize;
+
+    while deque.len() > 0 {
+        let current = deque.pop_front().unwrap();
+        if grid[current.0][current.1] == 9 {
+            trails += *seen.get(&current).unwrap();
+        }
+
+        // look at all the neighbors
+        for neighbor in neighbors(&current, &grid) {
+            // if the neighbor is out of bounds, continue
+            if neighbor.0 >= grid.len() || neighbor.1 >= grid[0].len() {
+                continue;
+            }
+
+            // if the next position is too far, continue
+            if grid[neighbor.0][neighbor.1] != grid[current.0][current.1] + 1 {
+                continue;
+            }
+
+            // if the neighbor has already been checked, continue
+            if seen.contains_key(&neighbor) {
+                *seen.get_mut(&neighbor).unwrap() += *seen.get(&current).unwrap();
+                continue;
+            }
+            seen.insert(neighbor, *seen.get(&current).unwrap());
+            deque.push_back(neighbor);
+
+            // println!(
+            //     "{:?}: {} -> {:?} : {}",
+            //     current, grid[current.0][current.1], neighbor, grid[neighbor.0][neighbor.1]
+            // );
+        }
+    }
+
+    trails
+}
+
+pub fn part_two(input: &str) -> Option<usize> {
+    let grid = input_to_matrix(input);
+
+    let trailheads = find_trailheads(&grid);
+
+    // println!("{:?}", trailheads);
+    let mut result = 0;
+    for trailhead in trailheads {
+        let score = score_unique_trails_to_trailend(&trailhead, &grid);
+        result += score;
+        // println!("Trailhead: {:?} Score: {}", trailhead, score);
+        // break;
+    }
+
+    Some(result)
 }
 
 #[cfg(test)]
@@ -125,6 +183,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(81));
     }
 }
