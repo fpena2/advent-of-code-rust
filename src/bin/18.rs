851fe082d81e9c2309cefaac89d7a73d3d8fbcbb
fpena2 <f.j.pena@wustl.edu>
2024-12-18 00:43:26 -0600

    d18

--- src/bin/18.rs
+++ src/bin/18.rs
@@ -1,14 +1,12 @@
 use std::collections::{HashSet, VecDeque};
 
 advent_of_code::solution!(18);
-const GRID_SIZE: usize = 70;
-const NUMB_OF_BYTES: usize = 1024;
 
-fn generate_grid() -> Vec<Vec<char>> {
-    vec![vec!['.'; GRID_SIZE + 1]; GRID_SIZE + 1]
+fn generate_grid(grid_size: usize) -> Vec<Vec<char>> {
+    vec![vec!['.'; grid_size + 1]; grid_size + 1]
 }
 
-fn process_input(input: &str) -> Vec<(usize, usize)> {
+fn get_bytes_coords(input: &str) -> Vec<(usize, usize)> {
     let mut points: Vec<(usize, usize)> = Vec::new();
     for line in input.lines() {
         let mut point = line.split(",");
@@ -18,9 +16,13 @@ fn process_input(input: &str) -> Vec<(usize, usize)> {
     }
     points
 }
+
 pub fn part_one(input: &str) -> Option<usize> {
-    let mut grid = generate_grid();
-    for (x, y) in process_input(input).iter().take(NUMB_OF_BYTES) {
+    const GRID_SIZE: usize = 6; // 70
+    const NUMB_OF_BYTES: usize = 12; // 1024
+
+    let mut grid = generate_grid(GRID_SIZE);
+    for (x, y) in get_bytes_coords(input).iter().take(NUMB_OF_BYTES) {
         grid[*x][*y] = '#';
     }
 
@@ -47,7 +49,6 @@ pub fn part_one(input: &str) -> Option<usize> {
             }
 
             if next_step.0 == next_step.1 && next_step.0 == GRID_SIZE {
-                println!("{}", dist + 1);
                 return Some(dist + 1);
             }
 
@@ -59,8 +60,67 @@ pub fn part_one(input: &str) -> Option<usize> {
     None
 }
 
-pub fn part_two(input: &str) -> Option<u32> {
-    None
+fn connnected(
+    grid: &Vec<Vec<char>>,
+    falling_bytes: &Vec<(usize, usize)>,
+    numb_of_bytes: usize,
+) -> bool {
+    let grid_size = grid.len() - 1;
+    let mut grid = grid.clone();
+    for (x, y) in falling_bytes.iter().take(numb_of_bytes) {
+        grid[*x][*y] = '#';
+    }
+
+    let mut queue: VecDeque<(usize, usize)> = VecDeque::from([(0, 0)]);
+    let mut seen: HashSet<(usize, usize)> = HashSet::from([(0, 0)]);
+    while queue.len() > 0 {
+        let (x, y) = queue.pop_front().unwrap();
+        for next_step in [
+            (x + 1, y),
+            (x.wrapping_sub(1), y),
+            (x, y + 1),
+            (x, y.wrapping_sub(1)),
+        ] {
+            if next_step.0 > grid_size || next_step.1 > grid_size {
+                continue;
+            }
+
+            if grid[next_step.0][next_step.1] == '#' {
+                continue;
+            }
+
+            if seen.contains(&next_step) {
+                continue;
+            }
+
+            if next_step.0 == next_step.1 && next_step.0 == grid_size {
+                return true;
+            }
+
+            seen.insert(next_step);
+            queue.push_back((next_step.0, next_step.1));
+        }
+    }
+
+    false
+}
+
+pub fn part_two(input: &str) -> Option<String> {
+    const GRID_SIZE: usize = 6; // 70;
+    let grid = generate_grid(GRID_SIZE);
+    let falling_bytes = get_bytes_coords(input);
+
+    let mut l = 0;
+    let mut r = falling_bytes.len() - 1;
+    while l < r {
+        let m = (l + r) / 2;
+        if connnected(&grid, &falling_bytes, m + 1) {
+            l = m + 1;
+        } else {
+            r = m;
+        }
+    }
+    Some(format!("{},{}", falling_bytes[l].0, falling_bytes[l].1))
 }
 
 #[cfg(test)]
@@ -76,6 +136,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some("6,1".to_string()));
     }
 }
