8149328226e3a0acc6350f130802a0f8379f4629
fpena2 <f.j.pena@wustl.edu>
2024-12-31 17:20:27 -0600

    refactor

--- src/bin/17.rs
+++ src/bin/17.rs
@@ -1,7 +1,7 @@
 advent_of_code::solution!(17);
 
 #[derive(Debug)]
-enum Opcode {
+enum OpCode {
     Adv,
     Bxl,
     Bst,
@@ -12,25 +12,107 @@ enum Opcode {
     Cdv,
 }
 
+impl From<u32> for OpCode {
+    fn from(prog: u32) -> Self {
+        match prog {
+            0 => OpCode::Adv,
+            1 => OpCode::Bxl,
+            2 => OpCode::Bst,
+            3 => OpCode::Jnz,
+            4 => OpCode::Bxc,
+            5 => OpCode::Out,
+            6 => OpCode::Bdv,
+            7 => OpCode::Cdv,
+            _ => panic!("Invalid Program value: {}", prog),
+        }
+    }
+}
+
+#[derive(Debug)]
+struct Register(u32);
+
+#[derive(Debug)]
+struct Registers {
+    a: Register,
+    b: Register,
+    c: Register,
+}
+
+impl Registers {
+    fn new(input: &str) -> Self {
+        let mut blocks = input.split("\n\n");
+        let regs_block = blocks.next().unwrap();
+        let mut registers = vec![];
+        for s in regs_block.lines() {
+            let parts: Vec<&str> = s.split(":").collect();
+            if parts.len() == 2 {
+                if let Ok(num) = parts[1].trim().parse::<u32>() {
+                    registers.push(num);
+                }
+            }
+        }
+
+        assert_eq!(registers.len(), 3);
+        Registers {
+            a: Register(registers[0]),
+            b: Register(registers[1]),
+            c: Register(registers[2]),
+        }
+    }
+}
+
+#[derive(Debug, Clone)]
+struct Program(u32);
+
+#[derive(Debug)]
+struct Programs(Vec<Program>);
+
+impl Programs {
+    fn new(input: &str) -> Self {
+        let mut blocks = input.split("\n\n").skip(1);
+        let prog_block = blocks.next().unwrap();
+        Programs(
+            prog_block
+                .replace("Program: ", "")
+                .split(",")
+                .filter_map(|s| Some(Program(s.trim().parse::<u32>().unwrap())))
+                .collect(),
+        )
+    }
+}
+
 #[derive(Debug)]
 struct Computer {
-    a: u32,
-    b: u32,
-    c: u32,
-    programs: Vec<u32>,
-    pc: u32,
+    registers: Registers,
+    programs: Programs,
+    pc: usize,
     output: Vec<u32>,
 }
 
 impl Computer {
-    fn run(&mut self) -> String {
-        let programs = self.programs.clone();
-        while self.pc < programs.len() as u32 {
-            let opcode = programs[self.pc as usize];
-            let operand = programs[self.pc as usize + 1];
+    fn new(input: &str) -> Self {
+        Computer {
+            registers: Registers::new(input),
+            programs: Programs::new(input),
+            pc: 0,
+            output: vec![],
+        }
+    }
+
+    fn run(&mut self) {
+        let programs_len = self.programs.0.len();
+        while self.pc < programs_len {
+            // First prog is the OpCode and second is the operand
+            let val_1 = self.programs.0[self.pc].0;
+            let val_2 = self.programs.0[self.pc + 1].0;
+
+            let opcode = OpCode::from(val_1);
+            let operand = val_2;
             self.calculate(opcode, operand);
         }
+    }
 
+    fn output(&self) -> String {
         self.output
             .iter()
             .map(|num| num.to_string())
@@ -38,110 +120,67 @@ impl Computer {
             .join(",")
     }
 
-    fn calculate(&mut self, opcode: u32, operand: u32) {
-        let combo = match operand {
+    fn combo(&self, operand: u32) -> u32 {
+        match operand {
             0..=3 => operand,
-            4 => self.a,
-            5 => self.b,
-            6 => self.c,
-            7 => panic!("Combo operand 7 is reserved"),
-            _ => unreachable!(),
-        };
+            4 => self.registers.a.0,
+            5 => self.registers.b.0,
+            6 => self.registers.c.0,
+            _ => panic!("Combo operands 7 and over are reserved"),
+        }
+    }
 
+    fn calculate(&mut self, opcode: OpCode, operand: u32) {
+        let combo = self.combo(operand);
         match opcode {
-            0 => {
-                // self.a = self.a / 2u32.pow(combo);
-                self.a = self.a >> combo;
+            OpCode::Adv => {
+                // same as a = a / (2 ^ combo)
+                self.registers.a.0 = self.registers.a.0 >> combo;
                 self.pc += 2;
             }
-            1 => {
-                self.b = self.b ^ operand;
+            OpCode::Bxl => {
+                self.registers.b.0 = self.registers.b.0 ^ operand;
                 self.pc += 2
             }
-            2 => {
-                self.b = combo % 8;
+            OpCode::Bst => {
+                self.registers.b.0 = combo % 8;
                 self.pc += 2;
             }
-            3 => {
-                if self.a > 0 {
-                    self.pc = operand;
+            OpCode::Jnz => {
+                if self.registers.a.0 > 0 {
+                    self.pc = operand as usize;
                 } else {
                     self.pc += 2
                 }
             }
-            4 => {
-                self.b = self.b ^ self.c;
+            OpCode::Bxc => {
+                self.registers.b.0 = self.registers.b.0 ^ self.registers.c.0;
                 self.pc += 2;
             }
-            5 => {
+            OpCode::Out => {
                 let output = combo % 8;
                 self.output.push(output);
                 self.pc += 2;
             }
-            6 => {
-                // self.b = self.a / 2u32.pow(combo);
-                self.b = self.a >> combo;
+            OpCode::Bdv => {
+                // same as b = a / (2 ^ combo)
+                self.registers.b.0 = self.registers.a.0 >> combo;
                 self.pc += 2;
             }
-            7 => {
-                // self.b = self.a / 2u32.pow(combo);
-                self.c = self.a >> combo;
+            OpCode::Cdv => {
+                // same as c = a / (2 ^ combo)
+                self.registers.c.0 = self.registers.a.0 >> combo;
                 self.pc += 2;
             }
-            _ => unreachable!(),
         };
-    }
-
-    pub fn get_programs(input: &str) -> Vec<u32> {
-        let mut blocks = input.split("\n\n").skip(1);
-        let prog_block = blocks.next().unwrap();
-        prog_block
-            .replace("Program: ", "")
-            .split(",")
-            .filter_map(|s| s.trim().parse::<u32>().ok())
-            .collect()
-    }
-
-    pub fn get_registers(input: &str) -> Vec<u32> {
-        let mut blocks = input.split("\n\n");
-        let regs_block = blocks.next().unwrap();
-        regs_block
-            .lines()
-            .filter_map(|s| {
-                let parts: Vec<&str> = s.split(":").collect();
-                if parts.len() == 2 {
-                    if let Ok(num) = parts[1].trim().parse::<u32>() {
-                        Some(num)
-                    } else {
-                        None
-                    }
-                } else {
-                    None
-                }
-            })
-            .collect()
-    }
-
-    fn new(input: &str) -> Self {
-        let registers = Self::get_registers(input);
-        let programs = Self::get_programs(input);
-
-        assert_eq!(registers.len(), 3);
-        Computer {
-            a: *registers.get(0).unwrap(),
-            b: *registers.get(1).unwrap(),
-            c: *registers.get(2).unwrap(),
-            programs,
-            pc: 0,
-            output: vec![],
-        }
     }
 }
 
 pub fn part_one(input: &str) -> Option<String> {
     let mut computer = Computer::new(input);
-    let output = computer.run();
-    Some(output)
+    computer.run();
+    let res = computer.output();
+    Some(res)
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
